78eb11648c90d2c8e283131891e1a66dd57fe731
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2021-01-15 22:11:52 +0200

    Added solution from coursera

--- src/main/kotlin/TaxiStuff.kt
+++ src/main/kotlin/TaxiStuff.kt
@@ -272,24 +272,19 @@ fun TaxiPark.checkParetoPrinciple(): Boolean {
                 .sumOf { it.second } >= 0.8
 }
 
-
-
-
-/*
-
 fun TaxiPark.checkParetoPrinciple1(): Boolean {
     if (trips.isEmpty()) return false
 
-    val totalIncome = trips.sumByDouble(TODO())
+    val totalIncome = trips.sumByDouble(Trip::cost)
     val sortedDriversIncome: List<Double> = trips
-        .groupBy(TODO())
-        .map { (_, tripsByDriver) -> tripsByDriver.sumByDouble(TODO()) }
+        .groupBy(Trip::driver)
+        .map { (_, tripsByDriver) -> tripsByDriver.sumByDouble (Trip::cost) }
         .sortedDescending()
 
     val numberOfTopDrivers = (0.2 * allDrivers.size).toInt()
     val incomeByTopDrivers = sortedDriversIncome
-        .take(TODO())
+        .take(numberOfTopDrivers)
         .sum()
 
     return incomeByTopDrivers >= 0.8 * totalIncome
-}*/
+}
