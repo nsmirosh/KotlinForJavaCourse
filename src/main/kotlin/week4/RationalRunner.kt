84d882d5a4308906c9dfa2aac50736c69959bfb1
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2021-02-08 18:13:35 +0200

    Implemented the working version of the Rational plus operator

--- src/main/kotlin/week4/RationalRunner.kt
+++ src/main/kotlin/week4/RationalRunner.kt
@@ -8,7 +8,12 @@ infix fun Int.divBy(toDivideBy: Int): Rational {
 }
 
 
-class Rational(rawData: String) {
+infix fun Long.divBy(toDivideBy: Long): Rational {
+    return Rational("$this/$toDivideBy")
+}
+
+
+class Rational(rawData: String) : Comparable<Rational> {
     var numerator: BigInteger
     var denominator: BigInteger
 
@@ -18,10 +23,39 @@ class Rational(rawData: String) {
             denominator = last().toBigInteger()
         }
     }
+
+    override fun compareTo(other: Rational): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (!(other is Rational)) {
+            return false
+        }
+        return other.numerator == this.numerator && other.denominator == this.denominator
+    }
+
+    override fun toString(): String {
+        return "numerator = $numerator, denominator = $denominator"
+    }
 }
 
-operator fun Rational.plus(otherRational: Rational): Rational {
-    return Rational("balls")
+operator fun Rational.plus(other: Rational): Rational {
+
+
+    val absHigherNumber=  this.denominator.max(other.denominator)
+    val absLowerNumber = this.denominator.min(other.denominator)
+    var lcm = absHigherNumber
+    while (lcm.mod(absLowerNumber).toInt() != 0) {
+        lcm += absHigherNumber
+    }
+    val thisToMultiplyBy = lcm / this.denominator
+    val otherToMultiplyBy = lcm / other.denominator
+
+    val sumOfNumerators = this.numerator * thisToMultiplyBy + other.numerator * otherToMultiplyBy
+
+    return Rational("$sumOfNumerators/$lcm")
 }
 
 operator fun Rational.minus(otherRational: Rational): Rational {
@@ -44,9 +78,6 @@ operator fun Rational.rangeTo(otherRational: Rational): Int {
     return 0
 }
 
-operator fun Rational.compareTo(otherRational: Rational): Int {
-    return 0
-}
 
 fun String.toRational(): Rational {
     return Rational(this)
@@ -56,9 +87,16 @@ fun main() {
     val half = 1 divBy 2
     val third = 1 divBy 3
 
-    val sum: Rational = half + third
-    println(5 divBy 6 == sum)
+    println(half)
+    println(third)
 
+
+    val sum: Rational = half + third
+
+    println(sum)
+    println(5 divBy 6)
+    println(5 divBy 6 == sum)
+/*
     val difference: Rational = half - third
     println(1 divBy 6 == difference)
 
@@ -76,14 +114,14 @@ fun main() {
     println("117/1098".toRational().toString() == "13/122")
 
     val twoThirds = 2 divBy 3
-    println(half < twoThirds)
+    println(half < twoThirds)*/
 
-    println(half in third..twoThirds)
+  /*  println(half in third..twoThirds)
 
     println(2000000000L divBy 4000000000L == 1 divBy 2)
 
     println(
         "912016490186296920119201192141970416029".toBigInteger() divBy
                 "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2
-    )
+    )*/
 }
