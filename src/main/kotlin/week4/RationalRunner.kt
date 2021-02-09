1e54a932434c4b179d6890ec083889184796679e
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2021-02-09 13:52:33 +0200

    Finished with everything except long and big integer comparison

--- src/main/kotlin/week4/RationalRunner.kt
+++ src/main/kotlin/week4/RationalRunner.kt
@@ -36,7 +36,12 @@ class Rational() : Comparable<Rational> {
     }
 
     override fun compareTo(other: Rational): Int {
-        TODO("Not yet implemented")
+        if ((this - other).numerator > BigInteger.ZERO) {
+            return 1
+        } else if ((this - other).numerator == BigInteger.ZERO) {
+            return 0
+        }
+        return -1
     }
 
     override fun equals(other: Any?): Boolean {
@@ -47,7 +52,12 @@ class Rational() : Comparable<Rational> {
     }
 
     override fun toString(): String {
-        return "numerator = $numerator, denominator = $denominator"
+        if (numerator / denominator > BigInteger.ONE) {
+            return "${numerator / denominator}"
+        }
+        numerator.gcd(denominator).apply {
+            return "${numerator / this}/${denominator / this}"
+        }
     }
 }
 
@@ -75,7 +85,7 @@ operator fun Rational.minus(other: Rational): Rational {
 
 
 fun Rational.getLCM(other: Rational): BigInteger {
-    val absHigherNumber=  this.denominator.max(other.denominator)
+    val absHigherNumber = this.denominator.max(other.denominator)
     val absLowerNumber = this.denominator.min(other.denominator)
     var lcm = absHigherNumber
     while (lcm.mod(absLowerNumber).toInt() != 0) {
@@ -84,21 +94,17 @@ fun Rational.getLCM(other: Rational): BigInteger {
     return lcm
 }
 
-operator fun Rational.times(otherRational: Rational): Rational {
-    return Rational("balls")
-}
+operator fun Rational.times(other: Rational): Rational =
+    Rational(this.numerator * other.numerator, this.denominator * other.denominator)
 
-operator fun Rational.div(otherRational: Rational): Rational {
-    return Rational("balls")
-}
+operator fun Rational.div(other: Rational): Rational =
+    Rational(this.numerator * other.denominator, this.denominator * other.numerator)
 
-operator fun Rational.unaryMinus(): Rational {
-    return Rational("balls")
-}
+operator fun Rational.unaryMinus(): Rational = Rational(-this.numerator, this.denominator)
 
-operator fun Rational.rangeTo(otherRational: Rational): Int {
+/*operator fun Rational.rangeTo(otherRational: Rational): Int {
     return 0
-}
+}*/
 
 
 fun String.toRational(): Rational {
@@ -116,7 +122,6 @@ fun main() {
     val difference: Rational = half - third
     println(1 divBy 6 == difference)
 
-/*
     val product: Rational = half * third
     println(1 divBy 6 == product)
 
@@ -128,17 +133,17 @@ fun main() {
 
     println((2 divBy 1).toString() == "2")
     println((-2 divBy 4).toString() == "-1/2")
-    println("117/1098".toRational().toString() == "13/122")
+    println("117/1098".toRational().toString() == "13/122") // divide numerator and denominator by gcm here
 
     val twoThirds = 2 divBy 3
-    println(half < twoThirds)*/
+    println(half < twoThirds)
 
-  /*  println(half in third..twoThirds)
+    println(half in third..twoThirds)
 
-    println(2000000000L divBy 4000000000L == 1 divBy 2)
-
-    println(
-        "912016490186296920119201192141970416029".toBigInteger() divBy
-                "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2
-    )*/
+      println(2000000000L divBy 4000000000L == 1 divBy 2)
+/*
+      println(
+          "912016490186296920119201192141970416029".toBigInteger() divBy
+                  "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2
+      )*/
 }
